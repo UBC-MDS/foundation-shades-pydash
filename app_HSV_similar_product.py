f1e65191eae3c8208378cc769762025045ea17a8
KangboLu <kblu@ucdavis.edu>
2021-01-19 00:16:24 -0800

    add div displaying user specified color

--- app_HSV_similar_product.py
+++ app_HSV_similar_product.py
@@ -1,3 +1,4 @@
+from colorutils import Color 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -113,22 +114,23 @@ app.layout = html.Div([
                     html.H6("Hue"),
                     dcc.Slider(
                         id='slider_hue', 
-                        min=0, max=360, step=1, 
-                        value=30,
-                        marks={ 0: '0°', 30: '30°' },
+                        min=0, max=50, step=1, 
+                        value=29,
+                        marks={0: '0°', 10: '10°', 20: '20°', 30: '30°', 
+                              40: '40°', 50: '50°'},
                     )
                 ],
-                className="three columns",
-                style={'paddingLeft': 0}
+                className="three columns"
             ),
             html.Div(
                 [
                     html.H6("Saturation"),
                     dcc.Slider(
                         id='slider_saturation',
-                        min=0, max=1, step=0.1,
-                        value=0.5,
-                        marks={0: '0', 0.5: '0.5', 1: '1'}
+                        min=0, max=1, step=0.01,
+                        value=0.53,
+                        marks={0: '0', 0.2: '0.2', 0.4: '0.4', 
+                               0.6: '0.6', 0.8: '0.8', 1: '1'}
                     )
                 ],
                 className="three columns"
@@ -138,9 +140,9 @@ app.layout = html.Div([
                     html.H6("Value/Brightness"),
                     dcc.Slider(
                         id='slider_value_brightness',
-                        min=0, max=1, step=0.1,
-                        value=0.5,
-                        marks={ 0: '0', 0.5: '0.5', 1: '1'}
+                        min=0.2, max=1, step=0.01,
+                        value=0.7,
+                        marks={ 0.2: '0.2', 0.6: '0.6', 1: '1'}
                     )
                 ],
                 className="three columns"
@@ -152,9 +154,10 @@ app.layout = html.Div([
     html.Div(
         [
             html.P(id='HSV_user_choice_output'),
+            html.Div(id="user_selected_color_div"),
             html.H6("Most Matching Results:")
         ],
-        className='five columns',
+        className='six columns',
     )
 ],
 className='ten columns offset-by-half')
@@ -162,13 +165,22 @@ className='ten columns offset-by-half')
 # callback for displaying user selection
 @app.callback(
     Output('HSV_user_choice_output', 'children'),
+    Output('user_selected_color_div', 'style'),
     Input('slider_hue', 'value'),
     Input('slider_saturation', 'value'),
     Input('slider_value_brightness', 'value'))
 def display_user_HSV_option(H, S, V):
+    # output string for displaying user choice
+    hex_color = Color(hsv=(H, S, V)).hex
     output_string = "You selected HSV value of H:" + \
-        str(H) + ", S: " + str(S) + ", and V: " + str(V)
-    return output_string
+        str(H) + ", S: " + str(S) + ", and V: " + str(V) + \
+        " (Hex code: " + hex_color + ")"
+    
+    # style change for div
+    style = {"backgroundColor": hex_color,
+            "height": "50px", 
+            "width": "100px"}
+    return output_string, style
 
 if __name__ == '__main__':
     app.run_server(debug=True)
